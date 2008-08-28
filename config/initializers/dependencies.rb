bf9ca34258e9eb495a43ce57e72813e64a39589b
Josh Nichols <josh@technicalpickles.com>
2008-08-28 14:05:19 -0400

    Removed dependencies initializer.

--- config/initializers/dependencies.rb
+++
@@ -1,7 +0,0 @@
-config.gem 'mislav-will_paginate', :version => '~>2.3', :lib => 'will_paginate', 
-  :source => 'http://gems.github.com/'
-config.gem 'jcnetdev-better_partials', :version => '>= 1.0',
-                                     :lib => 'better_partials',
-                                     :source => 'http://gems.github.com'
-config.gem 'factory_girl'
-config.gem 'shoulda-rails', :lib => 'shoulda/rails'
